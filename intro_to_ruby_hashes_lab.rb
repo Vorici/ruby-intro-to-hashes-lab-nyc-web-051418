f08d06a0f704d7576f1aa79f2802474f6cede93e
Ledio Bode <l.bode515@gmail.com>
2018-05-14 01:55:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,14 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	                 pieces: 4, 
-	                 names: {},
-	                 rent_in_dollars: {
-	                                   one_piece_owned: 2,
-	                                   two_pieces_owned: 50, 
-	                                   three_pieces owned: 200}
+	                 :names => {
+	                            :reading_railroad => {}
+	                 
+	                 },
+	                 :rent_in_dollars => {
+	                                       one_piece_owned: 2,
+	                                       two_pieces_owned: 50, 
+	                                       three_pieces owned: 200}
 	    
 	  }
 	}
