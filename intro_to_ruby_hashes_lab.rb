10f5dad6a71f4314d43e51d962c12b5d9aa6d71c
Ledio Bode <l.bode515@gmail.com>
2018-05-14 02:05:14 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ monopoly = {
 	                 :names => {
 	                            :reading_railroad => {}
 	                            :pennsylvania_railroad => {}
+	                            :b_and_o_railroad => {}
 	                 },
 	                 :rent_in_dollars => {
 	                                       one_piece_owned: 2,
